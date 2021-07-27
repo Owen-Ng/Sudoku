1262962b3b6fd04959bbbd8685022020fc91db08
Owen Ng <nigel.ngyanhing@mail.utoronto.ca>
2021-07-26 23:25:01 -0400

    Mostly Done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -30,34 +30,22 @@ cursor: pointer;
 function App() {
 
   const [Table, setTable] = useState<number[][]>([[0, 5, 0, 0, 9, 7, 0, 1, 0], [0, 0, 0, 0, 4, 5, 7, 0, 0], [0, 0, 0, 0, 3, 0, 2, 4, 0], [0, 0, 2, 0, 5, 8, 6, 0, 7], [0, 0, 0, 3, 7, 9, 0, 0, 0], [0, 0, 7, 0, 0, 2, 0, 5, 0], [5, 0, 0, 0, 8, 0, 0, 0, 0], [0, 7, 0, 9, 0, 0, 5, 0, 0], [9, 0, 3, 5, 6, 1, 8, 7, 2]])
-  const [Wrongs, setWrongs] = useState<number[][]>();
-  const [Goods, setGoods] = useState<number[][]>();
-  const [updatedTable, setupdatedTable] = useState<number[][]>([[0, 5, 0, 0, 9, 7, 0, 1, 0], [0, 0, 0, 0, 4, 5, 7, 0, 0], [0, 0, 0, 0, 3, 0, 2, 4, 0], [0, 0, 2, 0, 5, 8, 6, 0, 7], [0, 0, 0, 3, 7, 9, 0, 0, 0], [0, 0, 7, 0, 0, 2, 0, 5, 0], [5, 0, 0, 0, 8, 0, 0, 0, 0], [0, 7, 0, 9, 0, 0, 5, 0, 0], [9, 0, 3, 5, 6, 1, 8, 7, 2]])
+  const [solveTable, setsolveTable] = useState<number[][]>()
 
 
   useEffect(() => {
     sudoku = new Sudoku(JSON.parse(JSON.stringify(Table)));
     sudoku.solveSudoku();
-
+    setsolveTable(sudoku.getBoard())
   }, [])
-  function setValue(row: number, col: number, val: number) {
-    let tmp = Table;
-    tmp[row][col] = val;
-    setTable(tmp);
-    sudoku.updateBoard(tmp);
 
-  }
   function Reset() {
     setTable(JSON.parse(JSON.stringify(sudoku.resetBoard())));
-    setWrongs([]);
-    setGoods([]);
   }
 
   function Solve() {
     // sudoku.solveSudoku();
     setTable(JSON.parse(JSON.stringify(sudoku.getBoard())))
-    setWrongs([]);
-    setGoods([]);
   }
   function Lvl(lvl: string) {
     fetch(`https://sugoku.herokuapp.com/board?difficulty=${lvl}`).then(res => {
@@ -68,30 +56,9 @@ function App() {
       sudoku.solveSudoku();
       sudoku.getBoard();
       sudoku.resetBoard();
-      console.log(sudoku.board)
-      setWrongs([]);
-      setGoods([]);
     })
   }
 
-
-  function Check() {
-    // sudoku.solveSudoku();
-    let wrongs: number[][] = []
-    let goods: number[][] = []
-    const tmp = sudoku.getBoard();
-    const empty = sudoku.findEmptySpot();
-    [...Array(empty.length).keys()].map(i => {
-      const current = empty[i];
-      if (Table[current[0]][current[1]] === tmp[current[0]][current[1]]) {
-        goods.push([current[0], current[1]])
-      } else {
-        wrongs.push([current[0], current[1]])
-      }
-    })
-    setWrongs(wrongs);
-    setGoods(goods);
-  }
 
 
   return (
@@ -100,10 +67,9 @@ function App() {
       <LvlButton onClick={() => Lvl('easy')} style={{ backgroundColor: '#7a7af2' }}>Easy</LvlButton>
       <LvlButton onClick={() => Lvl('medium')} style={{ backgroundColor: 'yellow' }}>Medium</LvlButton>
       <LvlButton onClick={() => Lvl('hard')} style={{ backgroundColor: 'red' }}>Hard</LvlButton>
-      <Board board={Table} setValue={setValue} Wrongs={Wrongs} Goods={Goods} />
+      <Board board={Table} solveBoard={solveTable} />
       <Button onClick={Reset}>Reset</Button>
       <Button onClick={Solve}>Solve</Button>
-      <Button onClick={Check}>Check</Button>
 
     </div>
   );
--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -3,10 +3,7 @@ import React from "react";
 
 interface IProps {
     board: number[][],
-    setValue: (row: number, col: number, val: number) => void,
-    Wrongs?: number[][],
-    Goods?: number[][],
-    updateboard?: number[][]
+    solveBoard?: number[][]
 }
 export const Board: React.FC<IProps> = (props) => {
     const { board } = props;
@@ -16,57 +13,76 @@ export const Board: React.FC<IProps> = (props) => {
         if (isNaN(parseInt(value)) || parseInt(value) < 1) {
             event.target.value = "";
         }
-        else {
-            props.setValue(key[0], key[1], parseInt(value, 10))
-        }
+        // console.log(key)
+        // if (props.solveBoard && props.solveBoard[key[0]][key[1]] === parseInt(value)) {
+        //     const cur: HTMLElement = document.getElementById(`[${key[0]},${key[1]}]`) as HTMLElement;
+        //     cur.style.backgroundColor = "green";
+        // } else {
+        //     const cur: HTMLElement = document.getElementById(`[${key[0]},${key[1]}]`) as HTMLElement;
+        //     cur.style.backgroundColor = "red";
+        // }
+
     }
 
-    function CheckWrong(row: number, col: number): any {
-        // console.log(props.Wrongs)
-        if (props.Wrongs) {
-            for (let i = 0; i < props.Wrongs.length; i++) {
-                if (JSON.stringify(props.Wrongs[i]) === JSON.stringify([row, col])) {
-                    return true
+    function highlighCheck() {
+        let redCount = 0;
+        let greenCount = 0;
+        [...Array(board.length).keys()].map(row => {
+            [...Array(board[row].length).keys()].map(col => {
+                const cur: HTMLInputElement = document.getElementById(`[${row},${col}]`) as HTMLInputElement;
+
+                if (cur) {
+
+
+                    if (props.solveBoard && props.solveBoard[row][col] === parseInt(cur.value)) {
+                        cur.style.backgroundColor = "green";
+                        greenCount += 1;
+                    } else {
+                        cur.style.backgroundColor = "red";
+                        redCount += 1
+                    }
+
+                    setTimeout(function () {
+                        cur.style.backgroundColor = "";
+                    }, 1000);
+
                 }
 
-            }
-        }
 
-        return false
+
+
+            })
+
+        }
+        )
+        if (redCount === 0 && greenCount > 0) {
+            alert("Nice You finished it")
+        }
     }
 
-    function CheckGood(row: number, col: number): any {
-        // console.log(props.Wrongs)
-        if (props.Goods) {
-            for (let i = 0; i < props.Goods.length; i++) {
-                if (JSON.stringify(props.Goods[i]) === JSON.stringify([row, col])) {
-                    return true
-                }
 
-            }
-        }
-
-        return false
-    }
-
-    console.log(CheckWrong(0, 0))
     return (
-        <table>
-            <tbody>
-                {[...Array(board.length).keys()].map(row => {
-                    return (
-                        <tr key={row + Math.random()}>
-                            {[...Array(board[row].length).keys()].map(col =>
+        <div>
+            <table >
+                <tbody>
+                    {[...Array(board.length).keys()].map(row => {
+                        return (
+                            <tr key={row + Math.random()}>
+                                {[...Array(board[row].length).keys()].map(col =>
 
-                                <td key={`[${row},${col}]`}>{board[row][col] === 0 || CheckWrong(row, col) || CheckGood(row, col) ? <input type="text" onChange={(event) => ChangeHandler(event, [row, col])}
-                                    style={CheckWrong(row, col) ? { width: 10, height: 25, backgroundColor: "red" } : CheckGood(row, col) ? { width: 10, height: 25, backgroundColor: "green" } : { width: 10, height: 25 }}
-                                    maxLength={1} /> : board[row][col]}</td>
-                            )}
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+                                    <td key={`[${row},${col}]`}>{board[row][col] === 0 ? <input id={`[${row},${col}]`} type="text" onChange={(event) => ChangeHandler(event, [row, col])}
+                                        style={{ width: 10, height: 25 }}
+                                        maxLength={1} /> : board[row][col]}</td>
+                                )}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+
+            <button onClick={highlighCheck}>Check</button>
+        </div>
+
     );
 
 }
